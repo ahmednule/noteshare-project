830829a2cc4031ce0e610040ff3881a1cb2852c7
maitha1-nule <nuleahmed6@gmail.com>
2024-06-09 00:21:35 +0300

    I've created the profile page to be the upload button, in which users will be able to upload files with. The styling is not soo great but i'm still implemnting more. Incase you want to do it, feel free

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,15 +1,76 @@
 <template>
-  <div>
-    <h1>Profile</h1>
-  </div>
+  <v-container class="main-container" fill-height>
+    <v-toolbar class="toolbar">
+      <v-toolbar-title class="toolbar-title">
+        Noteshare
+        <v-toolbar-subtitle class="toolbar-caption">
+          Sharing Knowledge worldwide
+        </v-toolbar-subtitle>
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-row align="center" justify="center" class="fill-height">
+      <v-col cols="12" md="8" lg="6">
+        <v-card class="center-card">
+          <v-card-title>
+            <h2>Share Only Verified Materials to avoid Barn</h2>
+          </v-card-title>
+          <v-card-subtitle>
+            By doing this you'll receive 10 points which can be used for Unlimited Downloads and Sharing
+          </v-card-subtitle>
+          <v-card-text>
+            <p>Profile page</p>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-chip class="status-chip">Upload</v-chip>
+            <v-chip class="status-chip">Details</v-chip>
+            <v-chip class="status-chip">Done</v-chip>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+.main-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding-top: 90px;
+}
 
+.toolbar {
+  width: 100%;
+  height: 90px;
+  background-color: #bec3df;
+  position: fixed;
+  top: 0;
+}
+
+.toolbar-title {
+  text-align: left;
+}
+
+.toolbar-caption {
+  display: block;
+  font-size: 14px;
+  color: #5f6368;
+  margin-top: 4px;
+}
+
+.center-card {
+  padding: 20px;
+  text-align: center;
+}
+
+.status-chip {
+  margin-right: 8px;
+}
 </style>
